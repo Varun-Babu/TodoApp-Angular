b68dbe30a25b2f84a0aab91a3c680dbadcb58930
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-29 07:36:44 +0530

    databinded

--- src/app/home-login/home-login.component.ts
+++ src/app/home-login/home-login.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-login.component.css']
 })
 export class HomeLoginComponent {
+  Username =""
+  Password=""
+
+  readValue = () =>
+  {
+    let data:any = {"Username":this.Username,"Password":this.Password}
+
+    if(this.Username=="admin" && this.Password == "123")
+    {
+      alert("valid")
+    }
+    else{
+      alert("invalid")
+    }
+  
+  }
 
 }
